01af5177ece9e602f549a4896f5db60bf42adb1d
BobHawler <102181473+BobHawler@users.noreply.github.com>
2023-07-08 10:59:38 +0600

    Div for rows and main.js structure updated

--- red_it_or_not/static_project/main.js
+++ red_it_or_not/static_project/main.js
@@ -1,12 +1,20 @@
+// View layer
+var view = new ol.View({
+    center: ol.proj.fromLonLat([20.5094, 54.7067]),
+    zoom: 11
+    })
+
+// Basemap layer
+var basemaplayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+    })
+
+// Layers array
+var layerArray = [basemaplayer]
+
+// Initiating map
 var map = new ol.Map({
     target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-    })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([20.5094, 54.7067]),
-        zoom: 11
-    })
+    view: view,
+    layers: layerArray    
 });
